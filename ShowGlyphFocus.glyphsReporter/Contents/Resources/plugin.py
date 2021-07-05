ab55af68a08d9da51247e29a7351499073dcd34d
rafalbuchner <rafal.buchner@gmail.com>
2021-07-05 13:03:13 +0200

    respect italic angle when focusing https://github.com/mekkablue/ShowGlyphFocus/issues/1

--- ShowGlyphFocus.glyphsReporter/Contents/Resources/plugin.py
+++ ShowGlyphFocus.glyphsReporter/Contents/Resources/plugin.py
@@ -15,6 +15,12 @@ from __future__ import division, print_function, unicode_literals
 import objc
 from GlyphsApp import *
 from GlyphsApp.plugins import *
+from Foundation import NSPoint, NSAffineTransform, NSAffineTransformStruct, NSRect
+import math
+
+
+	
+
 
 class ShowGlyphFocus(ReporterPlugin):
 
@@ -28,6 +34,25 @@ class ShowGlyphFocus(ReporterPlugin):
 		})
 	
 	@objc.python_method
+	def transform(self, shiftX=0.0, shiftY=0.0, rotate=0.0, skew=0.0, scale=1.0):
+		myTransform = NSAffineTransform.transform()
+		if rotate:
+			myTransform.rotateByDegrees_(rotate)
+		if scale != 1.0:
+			myTransform.scaleBy_(scale)
+		if not (shiftX == 0.0 and shiftY == 0.0):
+			myTransform.translateXBy_yBy_(shiftX,shiftY)
+		if skew:
+			skewStruct = NSAffineTransformStruct()
+			skewStruct.m11 = 1.0
+			skewStruct.m22 = 1.0
+			skewStruct.m21 = math.tan(math.radians(skew))
+			skewTransform = NSAffineTransform.transform()
+			skewTransform.setTransformStruct_(skewStruct)
+			myTransform.appendTransform_(skewTransform)
+		return myTransform
+
+	@objc.python_method
 	def background(self, layer):
 		NSColor.colorWithRed_green_blue_alpha_(0.5, 0.5, 0.5, 0.2).set()
 		grayRect = NSRect( 
@@ -35,6 +60,13 @@ class ShowGlyphFocus(ReporterPlugin):
 			NSSize(layer.width,layer.master.ascender-layer.master.descender),
 			)
 		focusField = NSBezierPath.bezierPathWithRect_(grayRect)
+
+		italicAngle = layer.master.italicAngle
+		skewOriginHeight = layer.master.xHeight/2
+		shiftX = math.tan( math.radians(italicAngle) ) * skewOriginHeight * -1
+		transform = self.transform(skew=italicAngle, shiftX=shiftX)
+
+		focusField.transformUsingAffineTransform_(transform)
 		focusField.appendBezierPath_( layer.completeBezierPath.bezierPathByReversingPath() )
 		focusField.fill()
 		
