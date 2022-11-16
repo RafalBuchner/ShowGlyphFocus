862a2c2281409cbbf8f9f9881b7b1bafd9a72bff
rafal <rafal.buchner@gmail.com>
2022-11-16 23:13:01 +0100

    combined with rsb and added context menu

--- ShowGlyphFocus.glyphsReporter/Contents/Resources/plugin.py
+++ ShowGlyphFocus.glyphsReporter/Contents/Resources/plugin.py
@@ -15,9 +15,9 @@ from __future__ import division, print_function, unicode_literals
 import objc
 from GlyphsApp import *
 from GlyphsApp.plugins import *
-from Foundation import NSPoint, NSAffineTransform, NSAffineTransformStruct, NSRect
+from AppKit import NSPoint, NSAffineTransform, NSAffineTransformStruct, NSRect, NSColor, NSBezierPath
 import math
-
+from vanilla import Window, Group, TextBox, CheckBox
 
 	
 
@@ -34,12 +34,75 @@ class ShowGlyphFocus(ReporterPlugin):
 		})
 		self.LoadPreferences()
 
+		viewWidth = 200
+		viewHeight = 140
+		self.contextualMenuView = Window((viewWidth, viewHeight))
+		self.contextualMenuView.group = Group((0, 0, viewWidth, viewHeight))
+		self.contextualMenuView.group.text = TextBox((10, 10, -10, -10), "Show Glyph Focus")#, callback=self.checkboxCallback)
+		self.contextualMenuView.group.showHighlight = CheckBox((10, 0, -10, -10), "show highlight", callback=self.checkboxCallback, value=self.showHighlight)
+		self.contextualMenuView.group.showRSB = CheckBox((10, 40, -10, -10), "show RSB", callback=self.checkboxCallback, value=self.showRSB)
+		self.contextualMenuView.group.showTriangle = CheckBox((10, 80, -10, -10), "show triangles", callback=self.checkboxCallback, value=self.showTriangle)
+		self.contextualMenuView.group.showWhileMetrics = CheckBox((10, 120, -10, -10), "work on Show Metrics", callback=self.checkboxCallback, value=self.showWhileMetrics)
+		
+		
+		self.generalContextMenus = [
+			{"view": self.contextualMenuView.group.getNSView()}
+		]
+
 	@objc.python_method
+	def checkboxCallback(self, sender):
+		if sender.getTitle() == "show highlight":
+			self.showHighlight = True if sender.get() else False
+		if sender.getTitle() == "show RSB":
+			self.showRSB = True if sender.get() else False
+		if sender.getTitle() == "show triangles":
+			self.showTriangle = True if sender.get() else False
+		if sender.getTitle() == "work on Show Metrics":
+			self.showWhileMetrics = True if sender.get() else False
+		Glyphs.font.currentTab.redraw()
+		self.SavePreferences()
+
+	@objc.python_method
+	def conditionsAreMetForDrawing(self):
+		"""
+		Only activate if text or pan (hand) tool are active.
+		"""
+		currentController = self.controller.view().window().windowController()
+		if currentController:
+			tool = currentController.toolDrawDelegate()
+			textToolIsActive = tool.isKindOfClass_( NSClassFromString("GlyphsToolText") )
+			if textToolIsActive: 
+				return True
+		return False
+
+	@objc.python_method
+	def SavePreferences( self):
+		try:
+			# write current settings into prefs:
+			Glyphs.defaults["com.mekkablue.ShowGlyphFocus.showHighlight"] = self.showHighlight
+			Glyphs.defaults["com.mekkablue.ShowGlyphFocus.showRSB"] = self.showRSB
+			Glyphs.defaults["com.mekkablue.ShowGlyphFocus.showWhileMetrics"] = self.showWhileMetrics
+			Glyphs.defaults["com.mekkablue.ShowGlyphFocus.showTriangle"] = self.showTriangle
+			return True
+		except:
+			import traceback
+			print(traceback.format_exc())
+			return False
+
 	def LoadPreferences( self ):
-		Glyphs.registerDefault( "com.mekkablue.ShowGlyphFocus.color", (0.5, 0.5, 0.5, 0.2) )
+		Glyphs.registerDefault( "com.mekkablue.ShowGlyphFocus.color", (0.0, 0.0, 1, 0.1) )
 		Glyphs.registerDefault( "com.mekkablue.ShowGlyphFocus.colorDarkMode", (0.9, 0.9, 0.9, 0.9) )
+		Glyphs.registerDefault( "com.mekkablue.ShowGlyphFocus.showWhileMetrics", False )
+		Glyphs.registerDefault( "com.mekkablue.ShowGlyphFocus.showRSB", True )
+		Glyphs.registerDefault( "com.mekkablue.ShowGlyphFocus.showHighlight", True )
+		Glyphs.registerDefault( "com.mekkablue.ShowGlyphFocus.showTriangle", True )
+		
 
 		self.color = Glyphs.defaults["com.mekkablue.ShowGlyphFocus.color"]
+		self.showHighlight = Glyphs.defaults["com.mekkablue.ShowGlyphFocus.showHighlight"]
+		self.showRSB = Glyphs.defaults["com.mekkablue.ShowGlyphFocus.showRSB"]
+		self.showWhileMetrics = Glyphs.defaults["com.mekkablue.ShowGlyphFocus.showWhileMetrics"]
+		self.showTriangle = Glyphs.defaults["com.mekkablue.ShowGlyphFocus.showTriangle"]
 
 		darkModeIsTurnedOn = NSUserDefaults.standardUserDefaults().stringForKey_('AppleInterfaceStyle') == "Dark"	
 
@@ -68,20 +131,148 @@ class ShowGlyphFocus(ReporterPlugin):
 		return myTransform
 
 	@objc.python_method
+	def drawLine(self, x1, y1, x2, y2, strokeWidth):
+		try:
+			path = NSBezierPath.bezierPath()
+			path.moveToPoint_(NSPoint(x1, y1))
+			path.lineToPoint_(NSPoint(x2, y2))
+			path.setLineWidth_(strokeWidth)
+			NSColor.separatorColor().colorWithAlphaComponent_(0.4).set()
+			# NSColor.keyboardFocusIndicatorColor().set()
+			# NSColor.redColor().set()
+			path.stroke()
+		except:
+			import traceback
+			print(traeback.format_exc())
+
+	@objc.python_method
+	def drawTriangle(self, x1, y1, x2, y2, x3, y3):
+		path = NSBezierPath.bezierPath()
+		path.moveToPoint_(NSPoint(x1, y1))
+		path.lineToPoint_(NSPoint(x2, y2))
+		path.lineToPoint_(NSPoint(x3, y3))
+		path.closePath()
+		NSColor.separatorColor().colorWithAlphaComponent_(0.4).set()
+		# NSColor.systemRedColor().colorWithAlphaComponent_(0.4).set()
+		# NSColor.keyboardFocusIndicatorColor().set()
+		path.fill()
+	
+	@objc.python_method
+	def italicShift(self, yPos, angle, xHeight):
+		'''
+		ITALIC OFFSET. TAKEN FROM MARK FROMBERG'S SMART PLUMBLINES
+		'''
+		offset = math.tan(math.radians(angle)) * xHeight/2
+		shift = math.tan(math.radians(angle)) * yPos - offset
+		return shift
+
+	@objc.python_method
+	def drawRSB(self, layer):
+		if self.showRSB:
+			angle = layer.italicAngle
+
+			xHeight = layer.master.xHeight
+			x1, y1 = layer.width, layer.ascender
+			x2, y2 = x1, layer.descender
+			
+			# draw line:
+			strokeWidth = 0.75 * self.getScale() ** -0.9
+			self.drawLine( 
+				x1 + self.italicShift(y1, angle, xHeight), y1 , 
+				x2 + self.italicShift(y2, angle, xHeight), y2, 
+				strokeWidth,
+				)
+			
+		# triangle:
+		if self.showTriangle:
+			triangleSize = 6.0 / self.getScale()  ** 1
+			self.drawTriangle(
+				layer.width/2-triangleSize + self.italicShift(y1+triangleSize, angle, xHeight), y1+triangleSize,
+				layer.width/2 + self.italicShift(y1, angle, xHeight), y1,
+				layer.width/2+triangleSize + self.italicShift(y1+triangleSize, angle, xHeight), y1+triangleSize,
+				)
+			self.drawTriangle(
+				x1/2-triangleSize + self.italicShift(y2-triangleSize, angle, xHeight), y2-triangleSize,
+				x1/2 + self.italicShift(y2, angle, xHeight), (y2),
+				x1/2+triangleSize + self.italicShift(y2-triangleSize, angle, xHeight), y2-triangleSize,
+				)
+	
+	@objc.python_method
+	def drawLSB(self, layer):
+		if self.showRSB:
+			angle = layer.italicAngle
+
+			xHeight = layer.master.xHeight
+			x1, y1 = 0, layer.ascender
+			x2, y2 = x1, layer.descender
+			
+			# draw line:
+			strokeWidth = 0.5 * self.getScale() ** -0.9
+			self.drawLine( 
+				x1 + self.italicShift(y1, angle, xHeight), y1 , 
+				x2 + self.italicShift(y2, angle, xHeight), y2, 
+				strokeWidth,
+				)
+		
+		# triangle:
+		if self.showTriangle:
+			triangleSize = 6.0 / self.getScale()  ** 1
+			self.drawTriangle(
+				layer.width/2-triangleSize + self.italicShift(y2, angle, xHeight), y2,
+				layer.width/2 + self.italicShift(y2+triangleSize, angle, xHeight), (y2+triangleSize),
+				layer.width/2+triangleSize + self.italicShift(y2, angle, xHeight), y2,
+				)
+			self.drawTriangle(
+				layer.width/2-triangleSize + self.italicShift(y1+triangleSize, angle, xHeight), y1+triangleSize,
+				layer.width/2 + self.italicShift(y1, angle, xHeight), y1,
+				layer.width/2+triangleSize + self.italicShift(y1+triangleSize, angle, xHeight), y1+triangleSize,
+				)
+
+	@objc.python_method
 	def background(self, layer):
-		NSColor.colorWithRed_green_blue_alpha_(*self.color).set()
-		focusRect = NSRect( 
-			NSPoint(0.0,layer.master.descender),
-			NSSize(layer.width,layer.master.ascender-layer.master.descender),
-			)
-		focusField = NSBezierPath.bezierPathWithRect_(focusRect)
+		if self.showWhileMetrics and not Glyphs.defaults["showMetrics"]: return 
+		currentTab = layer.parent.parent.currentTab
+		if self.conditionsAreMetForDrawing():
+			italicAngle = layer.master.italicAngle
+			skewOriginHeight = layer.master.xHeight/2
+			shiftX = math.tan( math.radians(italicAngle) ) * skewOriginHeight * -1
+			transform = self.transform(skew=italicAngle, shiftX=shiftX)
+			if self.showHighlight:
+				NSColor.colorWithRed_green_blue_alpha_(*self.color).set()
+				focusRect = NSRect( 
+					NSPoint(0.0,layer.master.descender),
+					NSSize(layer.width,layer.master.ascender-layer.master.descender),
+					)
+				focusField = NSBezierPath.bezierPathWithRect_(focusRect)
 
-		italicAngle = layer.master.italicAngle
-		skewOriginHeight = layer.master.xHeight/2
-		shiftX = math.tan( math.radians(italicAngle) ) * skewOriginHeight * -1
-		transform = self.transform(skew=italicAngle, shiftX=shiftX)
+				
 
-		focusField.transformUsingAffineTransform_(transform)
-		focusField.appendBezierPath_( layer.completeBezierPath.bezierPathByReversingPath() )
-		focusField.fill()
+				focusField.transformUsingAffineTransform_(transform)
+				focusField.appendBezierPath_( layer.completeBezierPath.bezierPathByReversingPath() )
+				focusField.fill()
+
+			
+			if currentTab.direction == 0 :
+				self.drawRSB(layer)
+			if currentTab.direction == 2 :
+				self.drawLSB(layer)
+
+	# @objc.python_method
+	# def conditionalContextMenus(self):
+
+	# 	# Empty list of context menu items
+	# 	contextMenus = []
+
+	# 	# Execute only if layers are actually selected
+	# 	if Glyphs.font.selectedLayers:
+	# 		layer = Glyphs.font.selectedLayers[0]
+			
+	# 		# Exactly one object is selected and it’s an anchor
+	# 		if len(layer.selection) == 1 and type(layer.selection[0]) == GSAnchor:
+					
+	# 			# Add context menu item
+	# 			contextMenus.append({"name": "Randomly move anchor", "action": self.randomlyMoveAnchor})
+
+	# 	# Return list of context menu items
+	# 	return contextMenus
 		
